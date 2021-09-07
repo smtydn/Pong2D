46f05c47dd755bf665bd853058ee942bb61ce909
smtydn <sametaydin1997@gmail.com>
2021-09-07 16:31:11 +0300

    Player prefab and PlayerMovement script added

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10;
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float translation = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(0, translation * speed, 0);
+    }
+}
